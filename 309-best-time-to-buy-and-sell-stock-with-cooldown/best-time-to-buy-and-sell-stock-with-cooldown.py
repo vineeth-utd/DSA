8909ef4103d3c0a2ef53b64a14b0cadcb8433cbc
Vineeth Subramaniam J <59217225+vineeth-utd@users.noreply.github.com>
2026-07-13 12:38:21 -0700

    Time: 12 ms (6.87%) | Memory: 21 MB (31.80%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        memo = [[-1 for _ in [0, 1]] for _ in range(n+2)]
+        print(memo)
+
+        def getMaxProfit(curr_idx, can_buy):
+            if curr_idx >= n:
+                return 0
+            
+            if memo[curr_idx][can_buy] != -1:
+                return memo[curr_idx][can_buy]
+            
+            print((curr_idx, can_buy))
+            skip_profit = getMaxProfit(curr_idx + 1, can_buy)
+
+            action_profit = 0
+
+            if can_buy:
+                action_profit = getMaxProfit(curr_idx + 1, 0) - prices[curr_idx]
+            else:
+                action_profit = getMaxProfit(curr_idx + 2, 1) + prices[curr_idx]
+
+            memo[curr_idx][can_buy] = max(skip_profit, action_profit)
+            return memo[curr_idx][can_buy]
+        
+        return getMaxProfit(0, 1)
